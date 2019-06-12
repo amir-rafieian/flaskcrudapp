ab749bb24bcb07aa022cead22faa7eb0cf0fc449
“amir-rafieian” <rafieian.amir@gmail.com>
2019-06-12 11:58:17 +0800

    modified:   Procfile modified:   config/prod.py modified:   run.py

--- config/prod.py
+++ config/prod.py
@@ -1,4 +1,6 @@
+import os
+
 DEBUG=False
 SECRET_KEY = 'topsecret' #used by flask to secure the data in forms, sessions, cookies, etc
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost/catalog_db'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS=False #This is just to supress some warning messages
